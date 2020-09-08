3685389c6c6f5425ad250b761a720734ffbf2b9d
Frédéric Passaniti <frederic.passaniti@opendatasoft.com>
2020-09-08 11:27:11 +0200

    update gh pages

--- public/index.js
+++ public/index.js
@@ -2435,18 +2435,29 @@
     appId: "XGE67QYGAR",
     apiKey: "4de1a710351e1933128b8010dbc2a327",
     indexName: "Codelibrary",
-    routing: true
+    hitsPerPage: 5,
+    routing: true,
+    searchFunction(helper) {
+      const container = document.querySelector("#algolia-results");
+      container.style.display = helper.state.query === "" ? "none" : "";
+      helper.search();
+    }
   });
   search.addWidget(instantsearch.widgets.searchBox({
     container: "#algolia-box",
+    reset: true,
     placeholder: "Search for content"
   }));
   search.addWidget(instantsearch.widgets.infiniteHits({
     container: "#algolia-results",
     templates: {
       empty: "No results",
-      item: "<em>{{title}}</em> : {{{_highlightResult.summary.value}}}"
+      item: "<hr/> {{{_highlightResult.title.value}}}<br/> {{{_highlightResult.summary.value}}} <br/>{{{_highlightResult.tags[0].value}}}"
     }
+  }));
+  search.addWidget(instantsearch.widgets.refinementList({
+    container: "#algolia-refine",
+    attributeName: "tags"
   }));
   search.start();
 
